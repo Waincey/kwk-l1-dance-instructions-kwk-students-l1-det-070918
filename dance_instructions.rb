714bfe8bb62d15442301da5b6834e33fdf5d33f0
Waincey <waincey.chan@gmail.com>
2018-07-10 18:16:47 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -28,7 +28,8 @@ def  lasso
   puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
 end
 
-def 
+def bust_a_move
+  puts ""
   
   
   
